ec358fc0ab3c841ac75beec23d775df3709aeaad
unknown <Js@.(none)>
2010-06-30 00:51:22 +0200

    Sov på denna....

---
+++ jens/tc/practice/PowerPlants.java
@@ -0,0 +1,21 @@
+
+public class PowerPlants {
+
+	public int minCost(String[] connectionCost, String plantList, int numPlants){
+		
+		
+		for (int i = 0; i < connectionCost.length; i++) {
+			if(plantList.charAt(i) == 'Y'){
+				//
+			}
+		}
+		
+		return 0;
+		
+	}
+	
+	public static void main(String[] args) {
+		new PowerPlants().minCost(new String[]{"024",  "203", "430"}, "YNN", 3);
+	}
+
+}
