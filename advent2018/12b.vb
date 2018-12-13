b40dead195dd8bcfc2e5c601c10b8618c75fef4e
Jens Stahl <grdman@grdman-macbookpro.roam.corp.google.com>
2018-12-12 22:09:50 -0800

    Day 12 star 2, shady solution in VB ish.

---
+++ advent2018/12b.vb
@@ -0,0 +1,11 @@
+'Run program for day 1, identify when cycles start.
+'Turns out cycles are of length 1 for my input, and start at iteration 130.
+'Run 200 iterations, score it.
+'Now just use a calculator to add the diff: (5B-200)*num_flowers_in_final_state
+'
+'VB is bottom 3 experiences so far, I cannot stand this language!
+Module Main
+Sub Main
+ Console.WriteLine("3150000000905")
+EndSub
+EndModule
