f8a733c99f79a2a02ed207c529a17a5d12c3d4a2
js <stahl.jens@gmail.com>
2019-12-05 22:17:31 -0800

    Slight day 6 refactor.

--- advent2019/advent06.hs
+++ advent2019/advent06.hs
@@ -10,35 +10,35 @@ makePair :: String -> (String, String)
 makePair s = (a,b)
   where [a,b] = splitOn ")" s
 
+-- Key = child, Value = parent
 makeParentMap :: HashMap String String -> [(String, String)] -> HashMap String String
 makeParentMap m [] = m
 makeParentMap m ((p,c):xs) = makeParentMap (HM.insert c p m) xs
 
-countSteps :: HashMap String String -> (String, String) -> Int
-countSteps m (_,child) = case x of
-         Just n -> (1::Int) + (countSteps m ("",n))
+countSteps :: HashMap String String -> String -> Int
+countSteps m child = case x of
+         Just n -> (1::Int) + (countSteps m n)
          Nothing -> 0
    where x = HM.lookup child m
 
-enumerateSteps :: HashMap String String -> (String, String) -> [String]
-enumerateSteps m (_,child) = case x of
-         Just n -> (n:(enumerateSteps m ("",n)))
+enumerateSteps :: HashMap String String -> String -> [String]
+enumerateSteps m child = case x of
+         Just n -> (n:(enumerateSteps m n))
          Nothing -> []
    where x = HM.lookup child m
 
 firstStar :: String -> Int
 firstStar s = sum steps
   where pairs = DL.map makePair (lines s)
-        hashMap = makeParentMap HM.empty pairs
-        steps = DL.map (countSteps hashMap) pairs
-        
+        parentMap = makeParentMap HM.empty pairs
+        steps = DL.map (countSteps parentMap) (DL.map snd pairs)
 
 secondStar :: String -> Int
 secondStar s = totalNodes - 2*intersectionNodes
   where pairs = DL.map makePair (lines s)
-        hashMap = makeParentMap HM.empty pairs
-        mySteps = enumerateSteps hashMap ("", "YOU")
-        santaSteps = enumerateSteps hashMap ("", "SAN")
+        parentMap = makeParentMap HM.empty pairs
+        mySteps = enumerateSteps parentMap "YOU"
+        santaSteps = enumerateSteps parentMap "SAN"
         totalNodes = (DL.length mySteps) + (DL.length santaSteps)
         intersectionNodes = DL.length (intersect santaSteps mySteps)
 
