bf59c7333c8abd0d3fc0e491ba523e8a1a940d50
Jens <stahl.jens@gmail.com>
2023-12-11 20:56:49 -0800

    Day 11.

--- advent2023/src/11.ts
+++ advent2023/src/11.ts
@@ -23,5 +23,51 @@ readFile().then((input) => {
 
 function solve(input) {
   let sum = 0;
+  const cols : any = new Set(), rows : any = new Set();
+  const planets : any= [];
+  for (let i=0 ; i < input.length; i++) {
+    let hasThing = false;
+    for (let j =0 ; j<input[i].length; j++) {
+      if (input[i].charAt(j) == '#') {
+        hasThing = true;
+        planets.push([j, i]);
+      }
+    }
+    if (!hasThing) {
+      rows.add(i);
+    }
+  }
+  for (let j =0 ; j<input[0].length; j++) {
+    let hasThing = false;
+    for (let i=0 ; i < input.length; i++) {
+      if (input[i].charAt(j) == '#')
+        hasThing = true;
+    }
+    if (!hasThing) {
+      cols.add(j);
+    }
+  }
+
+
+  for (let a = 0; a < planets.length; a++) {
+    for (let b = a+1; b < planets.length; b++) {
+      const ap = planets[a];
+      const bp = planets[b];
+      let dist =0 ;
+      for(let x = Math.min(ap[0], bp[0]); x < Math.max(ap[0], bp[0]); x++) {
+        const price = cols.has(x) ? 1000000 : 1;
+        dist += price;
+      }
+      for(let y = Math.min(ap[1], bp[1]); y < Math.max(ap[1], bp[1]); y++) {
+        const price = rows.has(y) ? 1000000 : 1;
+        dist += price;
+      }
+      sum += dist;
+      console.log(ap, bp, dist);
+    }
+  }
+
+
+  console.log(cols, rows);
   return sum;
 }
